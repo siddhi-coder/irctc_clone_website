573d3fd1ebe13b0290233d717e58d546699ce5d8
Siddhi gada <83527318+siddhi-coder@users.noreply.github.com>
2023-09-14 13:37:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.getElementById("register").addEventListener("submit", (event) => {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Store the values in local storage
+//Store the values in local storage
     localStorage.setItem("username", username);
     localStorage.setItem("password", password);
 
-    // Redirect to the login page after registration
+    // Redirect to the login page after the registration
     window.location.href = "Login_Page/index.html";
 });
